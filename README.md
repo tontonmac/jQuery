27677214e3538c566dcd149f207b741524bb26a8
Mohamed Camara <ma.camara@hotmail.com>
2019-08-04 11:06:54 -0400

    done

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ fadeIn() -- hides DOM by fading in
 delay() -- delays the next method execution  
 slideUp() -- slide DOM up from shown state  
 slideDown() -- slides DOM down from hidden state
+$('element').text() --  know as getter (get the value)    
+$('element').html(); -- known as setter (set the value)  
--- project-2/js/app.js
+++ project-2/js/app.js
@@ -1,13 +1,27 @@
-$('#flashMessage').hide();
-$('#flashMessage').fadeIn(2000);
-$('flashMessage').delay(3000);
-$('#flashMessage').slideUp();
-$('#flashMessage').slideDown();
+// $('#flashMessage').hide();
+// $('#flashMessage').fadeIn(2000);
+// $('flashMessage').delay(3000);
+// $('#flashMessage').slideUp();
+// $('#flashMessage').slideDown();
 
-// short hand method
+// const title = 'My first Blog Post';
+// const content = 'This is my <strong>first</strong> post';
+
+// $('#blogTitlePreview').text(title);
+// $('#blogContentPreview').html(content);
+
 $('#flashMessage')
 .hide()
+
+$('#previewButton').click(function() {
+  const title = $('#blogTitleInput').val();
+  const content = $('#blogContentInput').val();
+
+  $('#blogTitlePreview').text(title);
+  $('#blogContentPreview').html(content);
+
+$('#flashMessage')
 .fadeIn(1000)
 .delay(2000)
 .slideUp()
-.slideDown();
+});
